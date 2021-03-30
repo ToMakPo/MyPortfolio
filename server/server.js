619be08843f3f98483f9200131cda413f767c2df
Makai Post <postmakai@gmail.com>
2021-03-30 15:05:32 -0700

    setup express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
 require("dotenv").config()
+const express = require("express")
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(process.env.NODE_ENV === "production" 
+    ? express.static("client/build")
+    : express.static("public")
+)
+
+app.listen(PORT, () => {
+    console.info(`App running on http://localhost:${PORT}`)
+})
