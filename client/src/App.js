4bc0117f03dba639ec8e7c09eed6fac502ec7278
Makai Post <postmakai@gmail.com>
2021-03-30 22:00:35 -0700

    got react working with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 // import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
 	return (<>
-		
+		<Home/>
 	</>)
 }
 export default App
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <section id='home-page'>
+            <h1>Makai Post</h1>
+            <sup>Software Developer</sup>
+        </section>
+    )
+}
+
+export default Home
