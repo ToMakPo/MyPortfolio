8e62cb10bfc190c38507535e6a8a86a226d80b05
Makai Post <postmakai@gmail.com>
2021-04-01 13:02:55 -0700

    about to uncommit back to client WITH server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-// import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import Admin from './data/Admin'
 
-const App = () => {
-	return (<>
-		<Home/>
-	</>)
+import './styles/App.css'
+
+function App() {
+	return (
+		<>
+		<Router>
+			<Switch>
+				<Route path='/admin' component={Admin} />
+				<Route render={_ => <Router>
+					<Switch>
+						<Route exact path='/' component={Home} />
+					</Switch>
+				</Router>} />
+			</Switch>
+		</Router>
+		</>
+	)
 }
+
 export default App
