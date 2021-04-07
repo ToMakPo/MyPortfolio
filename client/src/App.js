ce57ea89ed0c2d819a573e327bbe9b874423b9f1
Makai Post <postmakai@gmail.com>
2021-04-07 13:30:08 -0700

    still trying to fix it

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,12 @@ import './styles/App.css'
 
 function App() {
 	return (
-		<>
-		{console.log('starting router')}
 		<Router>
-			{console.log('starting swich')}
 			<Switch>
-				{console.log('checking /admin')}
-				<Route path='/admin' render={_ => {
-					console.log('going to admin');
-					return <Admin/>
-				}} />
-				{console.log('not /admin')}
-				<Route render={_ => <Router>
-					<Switch>
-						{console.log('checking /')}
-						<Route exact path='/' component={Home} />
-					</Switch>
-				</Router>} />
+				<Route exact path='/' component={Home}/>
+				<Route exact path='/admin' component={Admin}/>
 			</Switch>
 		</Router>
-		</>
 	)
 }
 
